3e275e02bdb683cd43f93feb8d3e3b1e91c2077f
Ertan_Mesut <ertan.mesut@turkcell.com.tr>
2021-01-10 13:00:54 +0300

    Docs: Update docstring of get_filters

--- bikeshare.py
+++ bikeshare.py
@@ -8,7 +8,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks user to specify a city, a month, and a day to analyze.
+    Asks end user to specify a city, a month, and a day to analyze.
 
     Returns:
         (str) city - name of the city to analyze
